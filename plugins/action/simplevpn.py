aeb1fd95c3bdb7fb9102339ff3a15f10e4c0dd29
Micah Anderson <micah@riseup.net>
2020-10-27 16:05:46 -0400

    Fix the secondary_gateway ip addition.
    The /etc/network/interfaces file may not have the right interface defined, which
    could cause an error using ansible's interfaces_file method.

--- plugins/action/simplevpn.py
+++ plugins/action/simplevpn.py
@@ -40,7 +40,7 @@ def produceEipConfig(config, obfs4_state_dir, public_domain, transports):
         "locations": config.locations,
         "gateways": [{
                 "host": "%s.%s" % (v["inventory_hostname"], public_domain),
-                "ip_address": v["gateway_address"],
+                "ip_address": v.get("gateway_address", v["ip"]),
                 "location": v.get("location", "Unknown"),
                 "capabilities": {
                     "adblock": False,
