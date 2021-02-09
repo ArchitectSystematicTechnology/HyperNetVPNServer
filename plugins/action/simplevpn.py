c7cfc558177bda36828a91f8da71905d4a6c4b75
Micah Anderson <micah@riseup.net>
2021-02-09 10:21:43 -0500

    indentation fix

--- plugins/action/simplevpn.py
+++ plugins/action/simplevpn.py
@@ -39,16 +39,16 @@ def produceEipConfig(config, obfs4_state_dir, public_domain, transports):
         "version": 3,
         "locations": config.locations,
         "gateways": [{
-                "host": "%s.%s" % (v["inventory_hostname"], public_domain),
-                "ip_address": v.get("gateway_address", v["ip"]),
-                "location": v.get("location", "Unknown"),
-                "capabilities": {
-                    "adblock": False,
-                    "filter_dns": False,
-                    "limited": False,
-                    "transport": transports,
-                },
-            } for v in config.gateways],
+            "host": "%s.%s" % (v["inventory_hostname"], public_domain),
+            "ip_address": v.get("gateway_address", v["ip"]),
+            "location": v.get("location", "Unknown"),
+            "capabilities": {
+                "adblock": False,
+                "filter_dns": False,
+                "limited": False,
+                "transport": transports,
+            },
+        } for v in config.gateways],
         "openvpn_configuration": config.openvpn,
     }
         
